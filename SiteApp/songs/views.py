4033f9d83dbaf18eaf0f0b4d5f9d15b51d7d224e
Effobless Deboffle <maxime.deboffle@hotmail.com>
2017-12-01 23:18:34 +0100

    Sauvegarde de modification d'autheurs ok

--- SiteApp/songs/views.py
+++ SiteApp/songs/views.py
@@ -1,8 +1,8 @@
-from .app import app
-from flask import render_template
+from .app import app, db
+from flask import render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from .models import get_sample, get_authors, get_author
+from .models import Author, get_sample, get_authors, get_author
 from wtforms import StringField, HiddenField
 from wtforms.validators import DataRequired
 
@@ -35,14 +35,38 @@ def authors():
         title = "Les Autheurs",
         authors = get_authors())
 
+@app.route("/one-author/<int:a>")
+def one_author(a):
+    author = get_author(a)
+    return render_template(
+        "one-author.html",
+        author = author)
+
+
 @app.route("/edit/author/<int:id>")
 def edit_author(id):
     a = get_author(id)
-    f = AuthorForm(id = a, name = a.name)
+    f = AuthorForm(id = a.id, name = a.name)
     return render_template(
                 "edit-author.html",
                 author = a,
                 form = f)
+
+@app.route("/save/author/", methods=("POST",))
+def save_author():
+    a = None
+    f = AuthorForm()
+    if f.validate_on_submit():
+        id = int(f.id.data)
+        a  = get_author(id)
+        a.name = f.name.data
+        db.session.commit()
+        return redirect(url_for('one_author', a = a.id))
+    a = get_author(int(f.id.data))
+    return render_template(
+            "edit-author.html",
+            author = a,
+            form = f)
 
 
 
